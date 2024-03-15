0202fdf97c9264f3276ced02d4815919a7dbb9d7
Alex Santos <devalexsantos@gmail.com>
2024-03-15 10:14:42 -0300

    feat: add footer layout

--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/Header'
 import { LocalBar } from './components/LocalBar'
+import { Footer } from './components/Footer'
 
 export default function AppLayout() {
   return (
     <div className="flex flex-col gap-3">
       <Header />
       <LocalBar />
-      <main className="container max-w-6xl">
+      <main className="container max-w-6xl min-h-[80vh]">
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
